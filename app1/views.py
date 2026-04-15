96c5ed1654ec4c8f21cbd89e65e47226dda1bee1
Unknown_8779 <erkinovashahlo230@gmail.com>
2026-04-15 19:43:32 +0500

    sqlite

--- app1/views.py
+++ app1/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from . import models
 
 # Create your views here.
 
 
 def hello(request):
-    return render(request,"app1/index.html")
+    # users=[
+    #     {'ism': 'Azizbek','familya': 'Abduraximov', 'yosh': 15},
+    #     {'ism': 'Anvar','familya': 'Xusniddinov', 'yosh': 14},
+    #     {'ism': 'Otabek','familya': 'Sobitjonov', 'yosh': 14},
+    # ]
+
+    users=models.User.objects.all()
+    return render(request,"app1/index.html",context={"odamlar": users})
