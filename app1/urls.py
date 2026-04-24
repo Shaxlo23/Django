82f446c775a553e6b54cfbb178eae41e18fe1839
Unknown_8779 <erkinovashahlo230@gmail.com>
2026-04-24 15:53:33 +0500

    link

--- app1/urls.py
+++ app1/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.hello)
+    path('',views.users_list,name='users_list'),
+    path('users/<int:user_id>/', views.user_view,name='user_view'),
 ]
--- app1/views.py
+++ app1/views.py
@@ -4,12 +4,10 @@ from . import models
 # Create your views here.
 
 
-def hello(request):
-    # users=[
-    #     {'ism': 'Azizbek','familya': 'Abduraximov', 'yosh': 15},
-    #     {'ism': 'Anvar','familya': 'Xusniddinov', 'yosh': 14},
-    #     {'ism': 'Otabek','familya': 'Sobitjonov', 'yosh': 14},
-    # ]
-
+def users_list(request):
     users=models.User.objects.all()
     return render(request,"app1/index.html",context={"odamlar": users})
+
+def user_view(request,user_id):
+    user=models.User.objects.get(pk=user_id)
+    return render(request,'app1/user_view.html',context={'user': user})
